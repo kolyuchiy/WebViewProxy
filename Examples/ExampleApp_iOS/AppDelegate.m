14288a78c75b95f28e96373a6bb5ff0b786d6ffb
Nikolay Morev <kolyuchiy@gmail.com>
2014-07-18 16:59:35 +0400

    Reproduce crash

--- Examples/ExampleApp_iOS/AppDelegate.m
+++ Examples/ExampleApp_iOS/AppDelegate.m
@@ -32,26 +32,9 @@
 }
 
 - (void) _setupProxy {
-    NSOperationQueue* queue = [[NSOperationQueue alloc] init];
-    [queue setMaxConcurrentOperationCount:5];
-    
-    [WebViewProxy handleRequestsWithHost:@"www.google.com" path:@"/images/srpr/logo3w.png" handler:^(NSURLRequest* req, WVPResponse *res) {
-        [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:req.URL] queue:queue completionHandler:^(NSURLResponse *netRes, NSData *data, NSError *netErr) {
-            if (netErr || ((NSHTTPURLResponse*)netRes).statusCode >= 400) { return [res respondWithError:500 text:@":("]; }
-            [res respondWithData:data mimeType:@"image/png"];
-        }];
-    }];
-    
-    [WebViewProxy handleRequestsWithHost:@"intercept" path:@"/Galaxy.png" handler:^(NSURLRequest* req, WVPResponse *res) {
-        NSString* filePath = [[NSBundle mainBundle] pathForResource:@"Galaxy" ofType:@"png"];
-        UIImage* image = [UIImage imageWithContentsOfFile:filePath];
-        [res respondWithImage:image];
-    }];
-    
-    [WebViewProxy handleRequestsWithHost:@"example.proxy" handler:^(NSURLRequest *req, WVPResponse *res) {
-        NSString* proxyUrl = [req.URL.absoluteString stringByReplacingOccurrencesOfString:@"example.proxy" withString:@"example.com"];
-        NSURLRequest* proxyReq = [NSURLRequest requestWithURL:[NSURL URLWithString:proxyUrl]];
-        [NSURLConnection connectionWithRequest:proxyReq delegate:res];
+    [WebViewProxy handleRequestsWithScheme:@"http" handler:^(NSURLRequest *req, WVPResponse *res) {
+        NSLog(@"%s %@", __PRETTY_FUNCTION__, req);
+        [NSURLConnection connectionWithRequest:req delegate:res];
     }];
 }
 
